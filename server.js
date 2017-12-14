4b9591ce273ec8896204007b8eceb0f7e874a00d
Mark Verbeck <MarkVerbeck@Marks-MacBook-Air.local>
2017-12-13 19:29:40 -0600

    word

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/reserve", function(req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+// Get all characters
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
