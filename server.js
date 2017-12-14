f15e1cdc324c6906eb128c687668de1c8d3efd6f
Lonnie Laub <llaub01@gmail.com>
2017-12-13 19:49:41 -0600

    routes done-ish

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+var reservation = require("./reservation.js");
 
 // global vars
 var app = express();
@@ -20,8 +21,22 @@ app.get("/reserve", function(req, res) {
   res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
-// Get all characters
 app.get("/tables", function(req, res) {
   res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
+// post
+app.post("/newres", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body-parser middleware
+  var newRes = req.body;
+  
+  //res won't have it's own page so we won't need dynamic routes to be added 
+  //newres.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newRes);
+
+  reservation.reservations.push(newRes);
+
+  res.json(newcharacter);
+});
